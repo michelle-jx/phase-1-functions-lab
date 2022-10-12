59eadf838e62e9698a1053db34fd4ef41e3fa8fa
Michelle Too <mjxtcodes@gmail.com>
2022-10-11 17:33:43 -0700

    commit tree 37bfa12668fe4a2caa0dfba5aa4b2ce640e452ae

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    const hq = 42;
+    const distanceTravelled = Math.abs(hq - distance);
+    return distanceTravelled;    
+}
+
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start)*264)
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination)
+    if (distanceInFeet > 400 && distanceInFeet < 2000) {
+        const farePrice = (distanceInFeet - 400) * .02
+        return farePrice;
+    } else if (distanceInFeet > 2500) {
+        return "cannot travel that far"
+    } else if (distanceInFeet > 2000) {
+        const farePrice2 = 25
+        return farePrice2;
+  } else if (distanceInFeet <= 400) {
+    return 0
+  }
+}
